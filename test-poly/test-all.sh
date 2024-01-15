3a22283486b80d02edaa0ef580b1c1fd70a8bff6
IQ8QI <bartekkonecki97@gmail.com>
2024-01-15 13:02:29 +0100

    improved test-all.sh it finnally works, one does not need to compile and link header with definitions

--- test-poly/test-all.sh
+++ test-poly/test-all.sh
@@ -2,27 +2,21 @@ echo "Compiling the tests!"
 
 FLAGS="-std=c++20 -Wall -Wpedantic"
 
-FILE1="./../polynomial-lib/polynomial.hpp"
-FILE1_RESULT="./polynomial_header.o"
+FILE1="./../polynomial-lib/polynomial.cpp"
+FILE1_RESULT="./polynomial.o"
 echo "Compiling $FILE1 into $FILE1_RESULT"
 g++ -c $FILE1 -o $FILE1_RESULT $FLAGS
 echo ""
 
-FILE2="./../polynomial-lib/polynomial.cpp"
-FILE2_RESULT="./polynomial.o"
+FILE2="./test-all.cpp"
+FILE2_RESULT="./test-all.o"
 echo "Compiling $FILE2 into $FILE2_RESULT"
-g++ $FILE1_RESULT $FILE2 -o $FILE2_RESULT $FLAGS
-echo ""
-
-FILE3="./test-all.cpp"
-FILE3_RESULT="./test-all.o"
-echo "Compiling $FILE3 into $FILE3_RESULT"
-g++ -c $FILE3 -o $FILE3_RESULT $FLAGS
+g++ -c $FILE2 -o $FILE2_RESULT $FLAGS
 echo ""
 
 RESULT="./test-all.app"
 echo "Linking the test executable into $RESULT"
-g++ $FILE1_RESULT $FILE2_RESULT $FILE3_RESULT -o $RESULT $FLAGS -lm
+g++ $FILE1_RESULT $FILE2_RESULT -o $RESULT $FLAGS
 echo ""
 
 echo "Running the tests!"
@@ -31,5 +25,5 @@ $RESULT
 echo ""
 
 echo "Cleaning up!"
-rm $FILE1_RESULT $FILE2_RESULT $FILE3_RESULT $RESULT
+rm $FILE1_RESULT $FILE2_RESULT $RESULT
 echo "Done!"
