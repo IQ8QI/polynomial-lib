38479836ef0765659bddccd9dbd1511b3ae81e2a
IQ8QI <bartekkonecki97@gmail.com>
2024-01-15 13:03:53 +0100

    added compare_double(), and other improvements to test-all.cpp

--- test-poly/test-all.cpp
+++ test-poly/test-all.cpp
@@ -1,31 +1,39 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 #include "./../polynomial-lib/polynomial.hpp"
 
 using namespace std;
 //using namespace poly;
 
-template<typename T1, typename T2>
-bool assert_eq(T1 ele1, T2 ele2, string failure){
-        if(ele1 == ele2){
+bool compare_double(double x1, double x2, int precision){
+        double difference = pow(10, -precision);
+        return abs(x1 - x2) < difference;
+}
+
+bool assert_eq(double x1, double x2, int precision, string& failure, size_t& test_no){
+        ++test_no;
+        if(compare_double(x1, x2, precision)){
                 return true;
         } else {
-                cout << failure << endl;
+                cout << failure << "in test no " << test_no << endl;
+                cout << x1 << " does not equal " << x2 << endl;
                 return false;
         }
 }
 
 void test_create_get_value(){
         string error = "ERROR in test_create_get_value()";
+        size_t no = 1;
         poly::poly my_poly = poly::poly({6.0, -2.0, 4.0});
-        assert_eq(my_poly.get_value(8), 246, error);
-        assert_eq(my_poly.get_value(-2.7894), 42.7018, error);
-        assert_eq(my_poly.get_value(42.7018), 7214.37, error);
+        assert_eq(my_poly.get_value(8), 246, 0, error, no);
+        assert_eq(my_poly.get_value(-2.7894), 42.7018, 4, error, no);
+        assert_eq(my_poly.get_value(42.7018), 7214.37, 2, error, no);
         vector<double> values = {4, -2.67, 7.13, -3.987};
         my_poly = poly::poly(values);
-        assert_eq(my_poly.get_value(42.7), -297516, error);
-        assert_eq(my_poly.get_value(-3), 183.829, error);
+        assert_eq(my_poly.get_value(42.7), -297516, 0, error, no);
+        assert_eq(my_poly.get_value(-3), 183.829, 3, error, no);
 }
 void test_show_read(){
         string error = "ERROR in test_create_get_value()";
